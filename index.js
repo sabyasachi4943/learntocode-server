1eba0b3f0aca6b3ba93f5883a9173bf23cc73fac
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-10-26 00:23:39 +0600

    topic detail

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
-const news = require("./data/content.json");
+const content = require("./data/content.json");
 
 app.get("/", (req, res) => {
   res.send("course api Running");
@@ -18,8 +18,8 @@ app.get("/course-categories", (req, res) => {
 
 app.get("/topics/:id", (req, res) => {
   const id = req.params.id;
-  const selectedNews = news.find((n) => n._id == id);
-  res.send(selectedNews);
+  const selectedContent = content.find((n) => n.category_id === id);
+  res.send(selectedContent);
 });
 
 app.listen(port, () => {
